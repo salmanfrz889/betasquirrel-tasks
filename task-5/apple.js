dfa08adaed440bee0059c0745b1473f52f23ed37
salmanul faris <salmanfrz889@gmail.com>
2024-07-01 17:44:59 +0530

    add task-5 completed

---
+++ task-5/apple.js
@@ -0,0 +1,160 @@
+//alert("Hello");
+
+//console.log("one");
+//console.info("two")
+//console.warn("three")
+//console.error("for")
+//console.debug("five")
+//console.clear("")
+
+/*
+one
+two
+*/
+
+// var firstName = "Salman"; // string
+// var lastName = "frz";  // string
+// let distanceFromOffice = 5.5;  // number
+// const isOnLeave = false; // boolean
+
+// firstName = "Aslam";  // reassign
+
+// distanceFromOffice = 6; // not edit
+// isOnLeave = true; //not edit
+
+// // let / const
+
+// console.log(firstName + " " + lastName)
+
+//let num1 = 5;
+//let num2 = 7;
+
+//console.log(typeof num1)
+
+// $ - jquery
+// _ - lodash/underscore
+
+//num1 = "6";
+
+//console.log(typeof num1)
+
+//function findSum(num1, num2) {
+      // parameters
+//let sum = num1 + num2;
+//alert(sum);
+//}
+
+    // call, execute,run, invoke
+//findSum(1, 3);  // arguments
+//findSum(8, 3);
+//function findSum(num1, num2)  {
+ //   return num1 + num2;
+//}
+//let sum = findSum(8, 3);
+//alert(sum);
+//console.log(sum)
+
+//let result = sum + 6;
+//console.log(result);
+
+
+//let age = 17
+//let result = age >= 18;
+//console.log(result);
+
+//if (result) {
+//    alert("Your application submitted");
+//} else {
+  //alert("You are not eligible at this age");  
+//}
+
+// let color = "yellow";
+// if(color === "orange") {
+//     console.log("You like to eat orange");
+// } else if(color === "yellow") {
+//     console.log("You like to eat banana");
+// } else {
+//     console.log("you don't like fruits");
+// }
+
+// DON - Document Object Model - DON Manipulation
+
+//const element = document.getElementById("color");
+//const color = element.value;
+//console.log(element)
+
+// getter, setter
+
+//--------------------
+// function findFavoriteFruit() {
+//     const color = document.getElementById("color").value;
+
+//      if (color === "orange") {
+//              console.log("You like to eat orange");
+//          } else if (color === "yellow") {
+//              console.log("You like to eat banana");
+//          } else {
+//              console.log("you don't like fruits");
+//          }
+
+// }
+//----------------------------
+
+// getter, setter
+        
+       /// Data Type//
+       
+ //---------------------------------
+
+         // string - "true", '5'
+         // number - 5, 5.5, -5
+         // boolean - true/ false
+         // undefined
+         // null
+         // bigint
+         // symbol
+         // object - object, array, date
+         // function
+//-------------------------
+
+//let a = null;
+//console.log(typeof a);
+  
+  /// [null oru object aan]
+
+
+  let student = {
+    firstName: "Aslam", 
+    lastName: "Muhsin", 
+    age: 22, 
+    isPassed: true};
+
+
+    // property
+    // methods / function
+
+    const car = {
+       model: "Nissan"
+       drive: function () {
+        console.log("boooom");
+       },
+    };
+
+    console.log(car.model);
+
+    // JSON -(Javascript Object Notation)
+
+    // XML
+     
+    // let grape;
+    // let apple;
+
+    // const student = { firstName: "Aslam" };
+
+    // student.lastName = "Muhsin";
+
+    // console.log(student);
+
+    const fruits = ["banana", "apple", 4, true];
+
+    console.log(fruits[fruits.length - 1]);
